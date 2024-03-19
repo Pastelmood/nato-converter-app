b18ea0990b01bdc4abeecb0ce2dd1f7fdeeabfbe
Chainarong Boonsangiem <0864713049@outlook.com>
2024-03-19 16:34:11 +0700

    Start Project

---
+++ src/main/java/online/meowplus/natoconverter/NatoConverterApplication.java
@@ -0,0 +1,13 @@
+package online.meowplus.natoconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NatoConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NatoConverterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/online/meowplus/natoconverter/NatoConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package online.meowplus.natoconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NatoConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
